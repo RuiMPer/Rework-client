a78a07c74f26a70d200c5b792acc3731dca726a3
Margarida Pascoal <margaridapascoal@Margaridas-MBP.lan>
2020-07-13 21:23:17 +0100

    navbar updated

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -27,22 +27,28 @@ class Navbar extends React.Component {
         return (
                 <>
                 <Nav pills>
-                    <NavItem>
-                        <NavLink exact to="/">Home</NavLink>
-                    </NavItem>
-                    <NavItem active={window.location.hash === '/services'}>
-                        <NavLink to="/services">All Services</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink to="/search">Search</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink to='/login'>Login</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink to='/signup'>Signup</NavLink>
-                    </NavItem>
-                    { this.props.loggedInUser ? ( <>
+
+                        <NavItem>
+                            <NavLink exact to="/">Home</NavLink>
+                        </NavItem>
+                    
+                    { !this.props.loggedInUser ? ( <>
+
+                        <NavItem active={window.location.hash === '/services'}>
+                            <NavLink to="/services">All Services</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to="/search">Search</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to='/login'>Login</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to='/signup'>Signup</NavLink>
+                        </NavItem>
+                        
+                    </> ) : ( <>
+
                         <NavItem to='/services'>Services
                         </NavItem>
                         <Dropdown nav onClick = { () => this.toggleActiveClass()} >
@@ -61,12 +67,13 @@ class Navbar extends React.Component {
                             <NavLink to="/profile">Profile</NavLink>
                         </NavItem>
                         <span>
-                            Welcome {this.props.loggedInUser.username}
+                            Welcome {this.props.loggedInUser.firstName}
                         </span>
                         <NavItem>
                             <NavLink to="/" onClick = { () => this.logoutUser()}>Logout</NavLink>
                         </NavItem>
-                    </> ) : ( <>
+
+
                     </> )}
                 </Nav>
             </>
