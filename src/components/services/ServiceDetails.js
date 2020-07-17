479ebf32f2dec82e53fbbe5767d30ac673bc0d5f
RuiMPer <rui.miguel.pereira.rp@gmail.com>
2020-07-17 08:10:36 +0100

    service view update

--- src/components/services/ServiceDetails.js
+++ src/components/services/ServiceDetails.js
@@ -24,6 +24,18 @@ class ServiceDetails extends Component {
         showAddBooking: false
     }
 
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            activeTab: '1'
+        };
+    }
+    toggle(tab) {
+        if (this.state.activeTab !== tab) {
+            this.setState({ activeTab: tab });
+        }
+    }
     getSingleService = () => {
         //id of the service is on the url /services/1234567
         const { params } = this.props.match;
@@ -59,63 +71,46 @@ class ServiceDetails extends Component {
     showAddBooking = () => {
         const { showForm, showInfo, showClient, showEditBooking } = this.state
 
-        this.state.showAddBooking ? this.setState({ showAddBooking: false }) : this.setState({ showAddBooking: true, showForm: false, showInfo: false, showClient: false, showEditBooking: false })
+        this.state.showAddBooking ? this.setState({ showAddBooking: false }) : this.setState({ showAddBooking: true, showBooking: false, showForm: false, showInfo: false, showClient: false, showEditBooking: false })
     }
-    showInfo = () => {
-        const { showForm, showBooking, showClient, showEditBooking } = this.state
+    // showInfo = () => {
+    //     const { showForm, showBooking, showClient, showEditBooking } = this.state
 
-        this.state.showInfo ? this.setState({ showInfo: false }) : this.setState({ showInfo: true, showAddBooking: false, showForm: false, showBooking: false, showClient: false, showEditBooking: false })
-    }
-    showBooking = () => {
-        const { showForm, showInfo, showClient, showEditBooking } = this.state
+    //     this.state.showInfo ? this.setState({ showInfo: false }) : this.setState({ showInfo: true, showAddBooking: false, showForm: false, showBooking: false, showClient: false, showEditBooking: false })
+    // }
+    // showBooking = () => {
+    //     const { showForm, showInfo, showClient, showEditBooking } = this.state
 
-        this.state.showBooking ? this.setState({ showBooking: false }) : this.setState({ showBooking: true, showForm: false, showInfo: false, showClient: false, showEditBooking: false })
-    }
-    showClient = () => {
-        const { showForm, showInfo, showBooking, showEditBooking } = this.state
+    //     this.state.showBooking ? this.setState({ showBooking: false }) : this.setState({ showBooking: true, showAddBooking: false, showForm: false, showInfo: false, showClient: false, showEditBooking: false })
+    // }
+    // showClient = () => {
+    //     const { showForm, showInfo, showBooking, showEditBooking } = this.state
 
-        this.state.showClient ? this.setState({ showClient: false }) : this.setState({ showClient: true, showForm: false, showInfo: false, showBooking: false, showEditBooking: false })
-    }
+    //     this.state.showClient ? this.setState({ showClient: false }) : this.setState({ showClient: true, showForm: false, showAddBooking: false, showInfo: false, showBooking: false, showEditBooking: false })
+    // }
     showEditBooking = () => {
         const { showForm, showInfo, showBooking, showClient } = this.state
 
-        this.state.showEditBooking ? this.setState({ showEditBooking: false }) : this.setState({ showEditBooking: true, showForm: false, showInfo: false, showBooking: false, showClient: false })
+        this.state.showEditBooking ? this.setState({ showEditBooking: false }) : this.setState({ showEditBooking: true, showAddBooking: false, showForm: false, showInfo: false, showBooking: false, showClient: false })
     }
 
 
     // 1. Happens first
     render() {
-        // const [activeTab, setActiveTab] = useState('1');
-
-        // const toggle = tab => {
-        //     if (activeTab !== tab) setActiveTab(tab);
-        // }
         const { params } = this.props.match;
         return (
             <div>
-                {/* <Nav tabs>
-                    <NavItem>
-                        <NavLink
-                            className={classnames({ active: activeTab === '1' })}
-                            onClick={() => { toggle('1'); }}
-                        >
-                            Tab1
-          </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink
-                            className={classnames({ active: activeTab === '2' })}
-                            onClick={() => { toggle('2'); }}
-                        >
-                            More Tabs
-          </NavLink>
-                    </NavItem>
-                </Nav> */}
-
                 <div>
                     <a href="/services">Back</a>
                     <p>{this.state.title}</p>
                 </div>
+                {this.props.loggedInUser &&
+                    <div>
+                        <button onClick={() => this.deleteService()}>Delete Service</button>
+                    </div>
+                }
+                <a onClick={() => this.showAddBooking()} href="#">Add Booking</a>
+                {this.state.showAddBooking && <AddBooking getService={this.getSingleService} serviceId={this.props.match.params.id} />}
                 <div>
                     <Link to={{
                         pathname: `/services/${params.id}/edit`,
@@ -126,31 +121,84 @@ class ServiceDetails extends Component {
                             photoPath: this.state.photoPath
                         }
                     }}>Edit Service</Link>
-                    <a onClick={() => this.showAddBooking()} href="#">Add Booking</a>
-                    {this.state.showAddBooking && <AddBooking getService={this.getSingleService} serviceId={this.props.match.params.id} />}
-
                 </div>
                 <div>
+                    <Nav tabs>
+                        <NavItem>
+                            <NavLink
+                                className={classnames({ active: this.state.activeTab === '1' })}
+                                onClick={() => { this.toggle('1'); }}
+                            >
+                                Information
+                        </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink
+                                className={classnames({ active: this.state.activeTab === '2' })}
+                                onClick={() => { this.toggle('2'); }}
+                            >
+                                Bookings
+                        </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink
+                                className={classnames({ active: this.state.activeTab === '3' })}
+                                onClick={() => { this.toggle('3'); }}
+                            >
+                                Clients
+                        </NavLink>
+                        </NavItem>
+                    </Nav>
+                    <TabContent activeTab={this.state.activeTab}>
+                        <TabPane tabId="1">
+                            {this.state.activeTab == 1 ? <div>
+                                <img src={this.state.photoPath} />
+                                <h3>Category</h3>
+                                <p>{this.state.category}</p>
+                                <h3>Description</h3>
+                                <p>{this.state.description}</p>
+                            </div> : null}
+                        </TabPane>
+                        <TabPane tabId="2">
+                            {this.state.activeTab == 2 ? <div>
+                                {this.state.bookings.map(booking => {
+                                    return (
+                                        <div key={booking._id}>
+                                            <ul>
+                                                <li>Title: {booking.title}</li>
+                                                <li>Description: {booking.description}</li>
+                                                <li>Date: {booking.date}</li>
+                                                <li>Time: {booking.time}</li>
+                                                <li>Client: {booking.client}</li>
+                                                <li><button onClick={this.showEditBooking}>Edit</button></li>
+                                                {/* {this.state.showForm && <EditBooking booking={booking} {...this.props} />} */}
+                                                {this.state.showEditBooking && <EditBooking booking={booking} {...this.props} />}
+                                            </ul>
+                                        </div>
+                                    )
+                                })}
+                            </div> : null}
+                        </TabPane>
+                        <TabPane tabId="3">
+                            {this.state.activeTab == 3 ? <h4>Tab 3 Contents</h4> : null}
+                        </TabPane>
+                    </TabContent>
+                </div>
+                {/* <div>
                     <a onClick={() => this.showInfo()} href="#">Information</a>
                     <a onClick={() => this.showBooking()} href="#">Bookings</a>
                     <a onClick={() => this.showClient()} href="#">Clients</a>
-                </div>
+                </div> */}
 
-                {this.state.showInfo && <div>
+                {/* {this.state.showInfo && <div>
                     <img src={this.state.photoPath} />
                     <h3>Category</h3>
                     <p>{this.state.category}</p>
                     <h3>Description</h3>
                     <p>{this.state.description}</p>
-                </div>}
+                </div>} */}
                 {/* <Example/> */}
 
-                {this.props.loggedInUser &&
-                    <div>
-                        <button onClick={() => this.deleteService()}>Delete Service</button>
-                    </div>
-                }
-                <hr />
                 <div>
                     {this.state.showBooking && this.state.bookings.map(booking => {
                         return (
@@ -168,9 +216,6 @@ class ServiceDetails extends Component {
                             </div>
                         )
                     })}
-                </div>
-                <div>
-
                 </div>
             </div>
         )
