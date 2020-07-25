c0a681176417c1ac250aa1e8d56c0d861cdbd1a8
RuiMPer <rui.miguel.pereira.rp@gmail.com>
2020-07-25 11:38:51 +0100

    fix

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -4,23 +4,23 @@ import { Link } from 'react-router-dom';
 import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 class Signup extends Component {
-    state = { username: '', password: '', firstName: '', lastName: '', email:'', type:'', errorMessage: '' };
+    state = { username: '', password: '', firstName: '', lastName: '', email: '', type: '', errorMessage: '' };
     service = new AuthService();
 
     handleFormSubmit = (event) => {
         event.preventDefault();
         console.log('type', this.state.type);
-        const {username, password, firstName, lastName, email, type} = this.state;
+        const { username, password, firstName, lastName, email, type } = this.state;
 
         this.service.signup(username, password, firstName, lastName, email, type)
             .then(response => {
                 this.setState({
-                    username: '', 
+                    username: '',
                     password: '',
                     firstName: '',
                     lastName: '',
                     email: '',
-                    type:''
+                    type: ''
                 });
 
                 this.props.setCurrentUser(response)
@@ -30,86 +30,87 @@ class Signup extends Component {
                 this.props.history.push("/")
             })
             .catch((error) => {
-                this.setState({errorMessage: error.response.data.message});
+                console.log(error.message)
+                // this.setState({ errorMessage: error.response.data.message });
             })
     }
 
-    handleChange = (event) => {  
-        const {name, value} = event.target;
-        this.setState({[name]: value, errorMessage:''});
+    handleChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value, errorMessage: '' });
     }
 
-    render(){
-        const {username, password, firstName, lastName, email, type} = this.state;
+    render() {
+        const { username, password, firstName, lastName, email, type } = this.state;
 
-        return( 
+        return (
             <>
-            <header className="header">
-                <h1>Signup</h1> 
-            </header>
+                <header className="header">
+                    <h1>Signup</h1>
+                </header>
 
-            <p className="text-info margin-content">Your personal informations</p><hr/>
-            <Form onSubmit={this.handleFormSubmit}>
-                <Row form>
-                    <Col md={6}>
-                        <FormGroup>
-                            <Label for="email">Email:</Label>
-                            <Input type="text" name="email" value={email} onChange={this.handleChange} placeholder="Escreve o teu email aqui..." />
-                        </FormGroup>
-                    </Col>
-                    <Col md={6}>
-                        <FormGroup>
-                            <Label for="password">Password:</Label>
-                            <Input name="password" type="password" value={password} onChange={this.handleChange} placeholder="&#9679; &#9679; &#9679; &#9679; &#9679;"/>
-                        </FormGroup>
-                    </Col>
-                </Row>
+                <p className="text-info margin-content">Your personal informations</p><hr />
+                <Form onSubmit={this.handleFormSubmit}>
+                    <Row form>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="email">Email:</Label>
+                                <Input type="text" name="email" value={email} onChange={this.handleChange} placeholder="Escreve o teu email aqui..." />
+                            </FormGroup>
+                        </Col>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="password">Password:</Label>
+                                <Input name="password" type="password" value={password} onChange={this.handleChange} placeholder="&#9679; &#9679; &#9679; &#9679; &#9679;" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
 
-                <Row form>
-                    <Col md={6}>
-                    <FormGroup>
-                        <Label for="username">First Name:</Label>
-                        <Input type="text" name="firstName" value={firstName} onChange={this.handleChange} placeholder="Escreve o teu primeiro nome aqui..." />
-                    </FormGroup>
-                    </Col>
-                    <Col md={6}>
-                    <FormGroup>
-                        <Label for="password">Last Name:</Label>
-                        <Input type="text" name="lastName" value={lastName} onChange={this.handleChange}/>
-                    </FormGroup>
-                    </Col>
-                </Row>
+                    <Row form>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="username">First Name:</Label>
+                                <Input type="text" name="firstName" value={firstName} onChange={this.handleChange} placeholder="Escreve o teu primeiro nome aqui..." />
+                            </FormGroup>
+                        </Col>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="password">Last Name:</Label>
+                                <Input type="text" name="lastName" value={lastName} onChange={this.handleChange} />
+                            </FormGroup>
+                        </Col>
+                    </Row>
 
-                <Row form>
-                    <Col md={6}>
-                        <FormGroup>
-                            <Label for="username">Username:</Label>
-                            <Input type="text" name="username" value={username} onChange={this.handleChange} placeholder="Escreve o teu username aqui..." />
-                        </FormGroup>
-                    </Col>
-                    <Col md={6}>
-                        <FormGroup>
-                            <Label for="typeofuser">Role</Label>
-                            <Input type="select" name="type" id="typeofuser" value={type} onChange={this.handleChange} >
-                                <option value="" disabled >Select your user type...</option>
-                                <option value="worker" id="worker" >Providing services</option>
-                                <option value="client" id="client" >Looking for services</option>
-                                {/* <option value="both" id="combo" >Both</option> */}
-                            </Input>
-                        </FormGroup>
-                    </Col>
-                </Row>
-                
-                {/* Displaying an error message */}
-                { this.state.errorMessage && <p className="error"> { this.state.errorMessage } </p> }
+                    <Row form>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="username">Username:</Label>
+                                <Input type="text" name="username" value={username} onChange={this.handleChange} placeholder="Escreve o teu username aqui..." />
+                            </FormGroup>
+                        </Col>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="typeofuser">Role</Label>
+                                <Input type="select" name="type" id="typeofuser" value={type} onChange={this.handleChange} >
+                                    <option value="" disabled >Select your user type...</option>
+                                    <option value="worker" id="worker" >Providing services</option>
+                                    <option value="client" id="client" >Looking for services</option>
+                                    {/* <option value="both" id="combo" >Both</option> */}
+                                </Input>
+                            </FormGroup>
+                        </Col>
+                    </Row>
 
-                <Button>Sign up</Button>
-                <FormGroup style={{marginTop: "10px"}}>
-                    <p>Already have account? 
+                    {/* Displaying an error message */}
+                    {this.state.errorMessage && <p className="error"> {this.state.errorMessage} </p>}
+
+                    <Button>Sign up</Button>
+                    <FormGroup style={{ marginTop: "10px" }}>
+                        <p>Already have account?
                         <Link to={"/login"}> Login</Link>
-                    </p>
-                </FormGroup>
-            </Form>
+                        </p>
+                    </FormGroup>
+                </Form>
             </>
         )
     }
