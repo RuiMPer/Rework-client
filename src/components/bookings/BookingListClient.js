356f459cf13ef2df7e57c3cad9ef7e3bc5158b92
Margarida Pascoal <margaridapascoal@Margaridas-MBP.lan>
2020-07-25 01:07:59 +0100

    bookings fixed

--- src/components/bookings/BookingListClient.js
+++ src/components/bookings/BookingListClient.js
@@ -7,7 +7,8 @@ import { CardHeader, CardBody, Jumbotron, Button, Card, CardTitle, CardText, Car
 class BookingListClient extends Component {
 
 	state = {
-		listOfServices: []
+		listOfServices: [],
+		bookings:[]
 	}
 
 	componentDidMount() {
@@ -33,45 +34,65 @@ class BookingListClient extends Component {
 	}
 	getUserBookings = () => {
 		this.getUserServices()
-
-		let today = moment().format("DD/MM/YYYY")
-		let eachBooking = []
-		this.state.listOfServices.map(response => {
-			if (response.bookings.length >= 1) {
-				response.bookings.map(response => {
-					console.log("ESTE", response)
-					let eachBook = axios.create({
-						baseURL: `${process.env.REACT_APP_SERVER}`,
-						withCredentials: true
-					});
-					eachBook.get(`/bookings/${response}`)
-						.then(response => {
-							console.log(response)
+		.then(()=>{
+			console.log("list of servicessssss",this.state.listOfServices)
+			let today = moment().format("DD/MM/YYYY")
+			let eachBooking = []
+			this.state.listOfServices.map(response => {
+				if (response.bookings.length >= 1) {
+					console.log("responseeeeeeeee",response.bookings)
+					response.bookings.map(response => {
+						console.log("ESTE", response)
+						let eachBook = axios.create({
+							baseURL: `${process.env.REACT_APP_SERVER}`,
+							withCredentials: true
 						});
-				});
-			}
-		});
+						eachBook.get(`/bookings/${response}`)
+							.then(response => {console.log("OLHAAAA",response.data.date)
+								console.log("FESTAAAAAAA", response)
+								console.log("OLHAAA2", moment.utc(response.data.date).utc().format('MM/DD/YYYY'))
+								let date = moment.utc(response.data.date).utc().format('MM/DD/YYYY');
+								let {confirmationStatus,
+									logStatus,
+									time,
+									title} = response.data
+
+								this.setState({
+									bookings:[{confirmationStatus,date:date, logStatus, time, title}]
+								});
+								// this.setState({
+								// 		bookings:[response.data]
+								// });
+							});
+					});
+				}
+			});
+		})
+		
 
 	}
 	render() {
+		let {bookings} = this.state;
 
 		return (
 			<>
 				<h1>My Bookings</h1>
 				<section className="bookinglist">
-					{this.state.listOfServices.map(response => {
+					{bookings.map((response) => {
 						
 						return (
 							<>
 								<Card key={response._id}>
-									<CardHeader tag="h3">{response.title}</CardHeader>
-									<CardHeader tag="h4">{response.date}</CardHeader>
-									<CardHeader tag="h4">{response.time}</CardHeader>
+									<span>
+										<CardHeader><strong>{response.title}</strong></CardHeader>
+										<CardBody>{response.date} | {response.time}</CardBody>
+									</span>
 									<Button>+</Button>
 								</Card>
 							</>
 						)
 					})}
+
 				</section>
 			</>
 		)
