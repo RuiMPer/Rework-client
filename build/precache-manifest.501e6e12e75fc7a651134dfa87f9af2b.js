2289ed8d4d12b4dd97e767301e6437e78e5feec5
RuiMPer <rui.miguel.pereira.rp@gmail.com>
2020-07-25 12:11:02 +0100

    fix

--- build/precache-manifest.501e6e12e75fc7a651134dfa87f9af2b.js
+++ build/precache-manifest.501e6e12e75fc7a651134dfa87f9af2b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1933ac6b41a26bebe571600f24aaf973",
+    "revision": "e85c305e40f7f8b6dc62f9ba8d3c86cf",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.0c5278e4.chunk.css"
   },
   {
-    "revision": "e57eb08b97f644ad1c6f",
+    "revision": "f223509aa8cdee3dddac",
     "url": "/static/css/main.292ff7e9.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.93426633.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e57eb08b97f644ad1c6f",
-    "url": "/static/js/main.8b09a910.chunk.js"
+    "revision": "f223509aa8cdee3dddac",
+    "url": "/static/js/main.3b0e04a9.chunk.js"
   },
   {
     "revision": "a56170de07fa57760c31",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6e2aa434c0bf35bfce3d879c43c7484.js"
+  "/precache-manifest.501e6e12e75fc7a651134dfa87f9af2b.js"
 );
 
 self.addEventListener('message', (event) => {
